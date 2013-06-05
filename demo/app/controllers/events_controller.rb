f1ca46c7106f05581c128a2cdb90ce88fa4c5fb2
Kenrick Zhou <Kenrick.Zhou@gmail.com>
2013-06-05 21:24:40 +0800

    learn curd

---
+++ demo/app/controllers/events_controller.rb
@@ -0,0 +1,17 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    @event.save
+
+    redirect_to :action => :index
+  end
+
+end
