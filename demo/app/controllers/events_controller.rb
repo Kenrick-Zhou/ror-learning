5ffe55314636164e68b536ce411bdc5080740cc2
Kenrick Zhou <Kenrick.Zhou@gmail.com>
2013-06-06 00:56:08 +0800

    complete the CRUD

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -1,40 +1,60 @@
 class EventsController < ApplicationController
+
+  before_filter :find_event, :only => [ :show, :edit, :update, :destroy]
+
+
   def index
-    @events = Event.all
+#    @events = Event.all
+    @events = Event.page(params[:page]).per(5)
   end
+
 
   def new
     @event = Event.new
   end
 
+
   def create
     @event = Event.new(params[:event])
-    @event.save
-
-    redirect_to :action => :index
+    if @event.save
+      flash[:notice] = "event was successfully created"
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
   end
 
+
   def show
-    @event = Event.find(params[:id])
     @page_title = @event.name
   end
 
+
   def edit
-    @event = Event.find(params[:id])
   end
+
 
   def update
-    @event = Event.find(params[:id])
-    @event.update_attributes(params[:event])
-
-    redirect_to :action => :show, :id => @event
+    if @event.update_attributes(params[:event])
+      flash[:notice] = "event was successfully updated"
+      redirect_to :action => :show, :id => @event
+    else
+      render :action => :edit
+    end
   end
 
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
 
+  def destroy
+    @event.destroy
+    flash[:alert] = "event was successfully deleted"
     redirect_to :action => :index
+  end
+
+
+  protected
+
+  def find_event
+    @event = Event.find(params[:id])
   end
 
 end
