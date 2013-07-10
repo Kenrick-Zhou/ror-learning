904228e32f8e8ad00c1e6f61e69269ff36fedc0a
Kenrick Zhou <Kenrick.Zhou@gmail.com>
2013-07-10 19:31:50 +0800

    get user_agent, client_ip, domain. for test

--- demo/app/controllers/welcome_controller.rb
+++ demo/app/controllers/welcome_controller.rb
@@ -3,6 +3,13 @@ class WelcomeController < ApplicationController
   end
 
   def index
+    @agent = request.env['HTTP_USER_AGENT']
+    @client_ip = request.remote_ip
+    if @client_ip == "218.246.76.5" 
+      @domain = "172.16.95.65"
+    else
+      @domain = "media.kaikeba.com"
+    end
   end
   
   def hell
